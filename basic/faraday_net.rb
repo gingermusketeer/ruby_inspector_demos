1f93f86468db3354677cfcc8e924e2be3e696876
Max Brosnahan <maximilianbrosnahan@gmail.com>
2015-02-21 15:11:29 +1100

    Extend faraday example with post and put requests

--- basic/faraday_net.rb
+++ basic/faraday_net.rb
@@ -12,9 +12,23 @@ RubyInspector.enable(
 binding.pry
 
 conn = Faraday.new(:url => 'http://localhost:9222') do |faraday|
+  faraday.request  :url_encoded
   faraday.response :logger                  # log requests to STDOUT
   faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
 end
 
 response = conn.get('json')
+puts "body length: #{response.body.size}"
+
+response = conn.post(
+  '/post_test', { :name => 'Steve' }
+)
+puts "body length: #{response.body.size}"
+
+
+response = conn.put do |req|
+  req.url '/put_test'
+  req.headers['Content-Type'] = 'application/json'
+  req.body = '{"name": "Steve"}'
+end
 puts "body length: #{response.body.size}"
