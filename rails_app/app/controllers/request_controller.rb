9e50a4550afbbfd9b598fc7ad3055cdadbeadc98
Max Brosnahan <maximilianbrosnahan@gmail.com>
2015-04-13 21:04:00 +1000

    Use local server

--- rails_app/app/controllers/request_controller.rb
+++ rails_app/app/controllers/request_controller.rb
@@ -4,20 +4,20 @@ require "uri"
 
 class RequestController < ApplicationController
   def get
-    example_com_html = open('http://example.com?param=value').read
+    example_com_html = open('http://localhost:9090/hello_world?param=value').read
     render html: example_com_html.html_safe
   end
 
   def post
     response = Net::HTTP.post_form(
-      URI.parse("http://httpbin.org/post"),
+      URI.parse("http://localhost:9090/post"),
       {"q" => "My query", "per_page" => "50"}
     )
     render json: response.body
   end
 
   def put
-    uri = URI('http://httpbin.org/put')
+    uri = URI('http://localhost:9090/put')
     http = Net::HTTP.new(uri.host, uri.port)
     req = Net::HTTP::Put.new(
       uri.path,
