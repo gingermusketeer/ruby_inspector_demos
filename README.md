fac0ce14e96163a306b32094b53037d13eff7a1d
Max Brosnahan <maximilianbrosnahan@gmail.com>
2015-04-13 20:23:02 +1000

    Add local http server #offline demos

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ This repo holds all the demos for [RubyInspector](https://github.com/gingermuske
 
 See demo folder for specific instructions
 
+# Setup
+- `bundle install` each of the demo apps
+- Run http server which will be hit by the demo apps `./http_server/bin/start`
+
 # Prerequisites
 
 - [RubyInspectorServer](https://github.com/gingermusketeer/ruby_inspector_server)
---
+++ http_server/server.js
@@ -0,0 +1,43 @@
+var express = require('express')
+var app = express()
+var fs = require('fs')
+
+app.get('/hello_world', function (req, res) {
+  fs.createReadStream('hello_world.html').pipe(res)
+})
+
+app.put('/put', function (req, res) {
+  var body = ''
+  req.on('data', function (data) {
+    body += data.toString()
+  })
+
+  req.on('end', function () {
+    res.send(JSON.stringify({
+      url: req.url,
+      headers: req.headers,
+      body: body
+    }))
+  })
+})
+
+app.post('/post', function (req, res) {
+  console.log('handling request')
+  var body = ''
+  req.on('data', function (data) {
+    console.log('got data')
+    body += data.toString()
+  })
+
+  req.on('end', function () {
+    console.log('end')
+    res.send(JSON.stringify({
+      url: req.url,
+      headers: req.headers,
+      body: body
+    }))
+  })
+})
+
+app.listen(9090)
+console.log('server listening on http://localhost:9090')
